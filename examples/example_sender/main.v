3019a5a6107ad31ffaf906fa793325b1b9288da8
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-22 20:48:44 +0900

    feat: implement osc sender example

--- examples/example_sender/main.v
+++ examples/example_sender/main.v
@@ -1,13 +1,16 @@
 module main
 
+import net
 import tinyosc
 
 fn main() {
+	mut sender := net.dial_udp('127.0.0.1:9000')!
+
 	mut buffer := []u8{cap: 2048}
 
 	v := f32(4.3 + 1.1)
 	len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"fif", 1.0, 2, v)
-	// len := tinyosc.tosc_write_message(buffer.data, sizeof(buffer), c"/hello", c"ifif", 5, 2.0, 4, 2.0)
-	// len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"")
 	C.tosc_printOscBuffer(buffer.data, len);
+
+	sender.write_ptr(buffer.data, int(len))!
 }
