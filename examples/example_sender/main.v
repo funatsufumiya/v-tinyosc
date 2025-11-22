87d4d9ec2fecc4e8c927d16c215b7ac31888bcf8
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-22 18:46:26 +0900

    feat: update example

--- examples/example_sender/main.v
+++ examples/example_sender/main.v
@@ -5,7 +5,7 @@ import tinyosc
 fn main() {
 	mut buffer := []u8{cap: 2048}
 
-	// len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"fi", 1.0, 2)
-	len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"")
+	len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"fi", 1.0, 2)
+	// len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"")
 	C.tosc_printOscBuffer(buffer.data, len);
 }
