7b2d418ef426221bf1b0cb47c3930e72e95e2074
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-22 17:07:56 +0900

    fix: update example

--- examples/example_sender/main.v
+++ examples/example_sender/main.v
@@ -6,7 +6,7 @@ fn main() {
 	mut buffer := []u8{cap: 2048}
 
 	// len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"fi", 1.0, 2)
-	len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"fi")
+	len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"")
 	C.tosc_printOscBuffer(buffer.data, len);
 
 	print("hello world")
