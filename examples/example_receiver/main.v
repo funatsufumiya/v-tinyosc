4d87316259bbc53bab0abe05c392917a1fd1b2dd
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-22 21:24:10 +0900

    chore: format codes

--- examples/example_receiver/main.v
+++ examples/example_receiver/main.v
@@ -4,30 +4,30 @@ import net
 import tinyosc
 
 fn main() {
-	raddr := "0.0.0.0:9000"
+	raddr := '0.0.0.0:9000'
 	mut receiver := net.listen_udp(raddr)!
-	println("OSC listening to ${raddr}")
+	println('OSC listening to ${raddr}')
 
 	mut buffer := []u8{len: 2048}
 	for {
 		len, _ := receiver.read(mut buffer) or { continue }
 
 		if C.tosc_isBundle(buffer.data) {
-      bundle := C.tosc_bundle_empty()
-      C.tosc_parseBundle(&bundle, buffer.data, len);
-      timetag := C.tosc_getTimetag(&bundle);
-      osc := C.tosc_message_empty()
-      for {
-        b := C.tosc_getNextMessage(&bundle, &osc)
-        if !b {
-          break
-        }
-        C.tosc_printMessage(&osc);
-      }
-    } else {
-      osc := C.tosc_message_empty()
-      C.tosc_parseMessage(&osc, buffer.data, len);
-      C.tosc_printMessage(&osc);
-    }
+			bundle := C.tosc_bundle_empty()
+			C.tosc_parseBundle(&bundle, buffer.data, len)
+			timetag := C.tosc_getTimetag(&bundle)
+			osc := C.tosc_message_empty()
+			for {
+				b := C.tosc_getNextMessage(&bundle, &osc)
+				if !b {
+					break
+				}
+				C.tosc_printMessage(&osc)
+			}
+		} else {
+			osc := C.tosc_message_empty()
+			C.tosc_parseMessage(&osc, buffer.data, len)
+			C.tosc_printMessage(&osc)
+		}
 	}
 }
--- examples/example_sender/main.v
+++ examples/example_sender/main.v
@@ -4,15 +4,15 @@ import net
 import tinyosc
 
 fn main() {
-	raddr := "127.0.0.1:9000"
+	raddr := '127.0.0.1:9000'
 	mut sender := net.dial_udp(raddr)!
-	println("OSC send to ${raddr}")
+	println('OSC send to ${raddr}')
 
 	mut buffer := []u8{cap: 2048}
 
 	v := f32(4.3 + 1.1)
-	len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c"/hello", c"fif", 1.0, 2, v)
-	C.tosc_printOscBuffer(buffer.data, len);
-
+	len := C.tosc_writeMessage(buffer.data, sizeof(buffer), c'/hello', c'fif', 1.0, 2,
+		v)
+	C.tosc_printOscBuffer(buffer.data, len)
 	sender.write_ptr(buffer.data, int(len))!
 }
